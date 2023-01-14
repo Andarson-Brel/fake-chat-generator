7b9b0b9cce7feb6d3e137290ae528e6a1eb0ed0e
Daniel Andara Innocent <106690294+Andarson-Brel@users.noreply.github.com>
2023-01-14 21:04:19 +0100

    added footer

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,6 +1,15 @@
 const menuIcon = document.querySelector(".plate");
 const navMenu = document.querySelector(".nav-menu ");
+const moreIcon = document.querySelector(".more-btn");
+const subMenu = document.querySelector(".sub-menu");
 
 menuIcon.addEventListener("click", function () {
   navMenu.classList.toggle("active");
+});
+moreIcon.addEventListener("click", function () {
+  if (window.innerWidth < 1000) {
+    subMenu.classList.toggle("mobile");
+  } else {
+    subMenu.classList.toggle("active");
+  }
 });
