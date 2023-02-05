fe548ee839fac8a042c02c566e793cc268f489cd
job kumdan <redeks123456@gmail.com>
2023-02-05 15:54:01 +0100

    finishing touches on instagram,twitter and facebook

--- javascript/instagram.js
+++ javascript/instagram.js
@@ -1,7 +1,6 @@
 "use strict";
-
-const Chat = function (typeOfChat, mess) {
-  return ` <div class="chat-${typeOfChat}">
+const Chat = function (typeOfChat, mess, id) {
+  return ` <div data-id="${id}" class="chat-${typeOfChat}">
   ${typeOfChat === "r" ? `<div class="sp"></div>` : ""}
   <div class="mess mess-${typeOfChat}">
     <p>
@@ -11,47 +10,70 @@ const Chat = function (typeOfChat, mess) {
   ${typeOfChat === "l" ? `<div class="sp"></div>` : ""}
 </div>`;
 };
+const chatFormat = function (id, typeOfChat) {
+  const el = document.querySelectorAll(`[data-id="${id}"]`);
+  el.forEach((e) => {
+    if (e.previousElementSibling) {
+      const parent1 = e.previousElementSibling;
+      const classType = parent1.classList.value;
+      if (classType === `chat-${typeOfChat}`) {
+        const child1 = parent1.querySelector(".mess");
+        const child2 = e.querySelector(".mess");
+
+        if (typeOfChat === "l") {
+          child1.style.borderRadius = " 20px 20px 20px 2px";
+        }
+        if (typeOfChat === "r") {
+          child1.style.borderRadius = " 20px 20px 2px 20px";
+        }
+        child1.style.marginBottom = "2px";
+        child2.style.marginTop = "0";
+        if (parent1.previousElementSibling) {
+          const classType2 = parent1.previousElementSibling.classList.value;
+          if (classType2 === `chat-${typeOfChat}`) {
+            if (typeOfChat === "l") {
+              child1.style.borderRadius = " 2px 20px 20px 2px";
+            }
+            if (typeOfChat === "r") {
+              child1.style.borderRadius = " 20px 2px 2px 20px";
+            }
+            child1.style.marginTop = "0";
+            child2.style.marginBottom = "2px";
+          }
+        }
+      }
+    }
+  });
+};
+const completeChat = function (typeOfChats, chatInput1, chatInput2) {
+  if (!chatInput1.value) return;
+
+  id++;
+  typeOfChat = typeOfChats;
+  mess = chatInput1.value;
+  generic(chatInput1, Chat(typeOfChat, mess, id), iChatMain);
+  generic(chatInput2, Chat(typeOfChat, mess, id), aChatMain);
+  chatFormat(id, typeOfChat);
+};
 
 iSend.addEventListener("click", function () {
   addImages(iSend);
-  if (!iChatInput.value) return;
-
-  typeOfChat = "r";
-  mess = iChatInput.value;
-  generic(iChatInput, Chat(typeOfChat, mess), iChatMain);
-  generic(aChatInput, Chat(typeOfChat, mess), aChatMain);
+  completeChat("r", iChatInput, aChatInput);
 });
 aSend.addEventListener("click", function () {
   addImages(aSend);
-  if (!aChatInput.value) return;
-
-  typeOfChat = "r";
-  mess = aChatInput.value;
-  generic(iChatInput, Chat(typeOfChat, mess), iChatMain);
-  generic(aChatInput, Chat(typeOfChat, mess), aChatMain);
+  completeChat("r", aChatInput, iChatInput);
 });
 
 if (iRecBtn) {
   iRecBtn.addEventListener("click", function (e) {
     e.preventDefault();
-    if (!recInput.value) return;
-
-    typeOfChat = "l";
-
-    mess = recInput.value;
-    generic(recInput, Chat(typeOfChat, mess), iChatMain);
-    generic(recInput, Chat(typeOfChat, mess), aChatMain);
+    completeChat("l", recInput, recInput);
   });
 }
 if (aRecBtn) {
   aRecBtn.addEventListener("click", function (e) {
     e.preventDefault();
-    if (!recInput.value) return;
-
-    typeOfChat = "l";
-
-    mess = recInput.value;
-    generic(recInput, Chat(typeOfChat, mess), iChatMain);
-    generic(recInput, Chat(typeOfChat, mess), aChatMain);
+    completeChat("l", recInput, recInput);
   });
 }
